00d91a94e262377bc06bc55fc8005e57638dd303
Augusto Barbosa <augusto.barbosawork@gmail.com>
2025-07-01 21:38:21 -0300

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,19 +3,18 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
 
-export const authenticate = (req, res, next) => {
+export function authenticate(req, res, next) {
   const authHeader = req.headers.authorization;
-  if (!authHeader)
-    return res.status(401).json({ message: 'Token não fornecido' });
+  if (!authHeader) return res.status(401).send({ message: 'Token não enviado' });
 
   const token = authHeader.split(' ')[1];
-
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET);
     req.store_id = decoded.store_id;
     next();
   } catch (error) {
-    return res.status(403).json({ message: 'Token inválido' });
+    return res.status(403).send({ message: 'Token inválido' });
   }
-};
+}
